bd4fd6e681b6fe27bc49737574bbbff3e5ae0a3b
Hervey John Lamothe <herveylamothe@gmail.com>
2018-12-29 17:27:38 -0500

    added multiplication route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 let app = express();
 
 app.get('/', (request, response) => {
-    
+
 
 
 
@@ -16,19 +16,74 @@ app.get('/math/add/', (request, response) => {
     let values = Object.values(request.query);
 
 
-    
+
     let arrToNum = [];
-        for(let i = 0; i<values.length; i++) {
-            arrToNum.push(parseInt (values[i]))
+    for (let i = 0; i < values.length; i++) {
+        arrToNum.push(parseInt(values[i]))
+    }
+    let sum = 0;
+    for (let i = 0; i < arrToNum.length; i++) {
+        sum = arrToNum[i] + sum
+    }
+
+    let sumString = '';
+    for (let i = 0; i < values.length; i++) {
+        if (i === 0) {
+            sumString = sumString.concat(values[i])
+        } else {
+            sumString = sumString.concat(' + ', values[i])
         }
-     let sum = 0;   
-        for(let i = 0; i<arrToNum.length; i++) {
-            sum = arrToNum[i] + sum
-        }
+        // sumString.push(parseInt (values[i]))
+    }
+    //  let sum = 0;   
+    //     for(let i = 0; i<sumString.length; i++) {
+    //         sum = string[i] + sum
+    //     }
 
     response.send({
         input: request.query,
+        sumString,
         sum
     });
 })
+
+
+
+app.get('/math/multiply/', (request, response) => {
+    // console.log(Object.values(request.query).map(e => parseInt(e)))
+
+    let values = Object.values(request.query);
+
+
+
+    let arrToNum = [];
+    for (let i = 0; i < values.length; i++) {
+        arrToNum.push(parseInt(values[i]))
+    }
+    let product = 1;
+    for (let i = 0; i < arrToNum.length; i++) {
+        product = arrToNum[i] * product
+    }
+
+    let productString = '';
+    for (let i = 0; i < values.length; i++) {
+        if (i === 0) {
+            productString = productString.concat(values[i])
+        } else {
+            productString = productString.concat(' * ', values[i])
+        }
+        // sumString.push(parseInt (values[i]))
+    }
+    //  let sum = 0;   
+    //     for(let i = 0; i<sumString.length; i++) {
+    //         sum = string[i] + sum
+    //     }
+
+    response.send({
+        input: request.query,
+        productString,
+        product
+    });
+})
+
 app.listen(4000);
